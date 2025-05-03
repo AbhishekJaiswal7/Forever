963704672ef8e7a63f08ad8561e81713b526a616
Abhishek <abhishekjaiswal1788@gmail.com>
2025-05-03 21:30:11 +0530

    Add cart-related functions to ShopContext

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -1,14 +1,65 @@
-import React, { createContext } from 'react'
+import React, { useState, createContext, useEffect } from 'react'
 import { products } from '../assets/assets' 
-
+import { toast } from 'react-toastify';
 export const ShopContext = createContext();
 
 const ShopContextProvider = (props) => {
     const currency = '$';
     const delivery_fee = 10;
+    const [search, setSearch] = useState('')
+    const [showSearch, setShowSearch] = useState(false)
+    const [cartItems, setCartItems] = useState({})
+
+    const addToCart = async (itemId, size) => {
+        console.log("addToCart called with", itemId, size);
+        if(!size){
+            toast.error('Please select a size')
+            return;
+        }
+
+        let cartData = structuredClone(cartItems)
+
+        if (cartData[itemId]){
+            if(cartData[itemId][size]){
+                cartData[itemId][size] += 1;
+            }
+            else{
+                cartData[itemId][size] = 1;
+            }
+        }
+        else{
+            cartData[itemId] = {}
+            cartData[itemId][size] = 1;
+        }
+
+        setCartItems(cartData)
+        console.log("Cart updated:", cartData)
+    } 
+
+    const getCartCount = () => {
+        let count = 0;
+        for (const item in cartItems) {
+            for (const size in cartItems[item]) {
+                count += cartItems[item][size]
+            }
+        }
+        return count
+    }
+
+    const updateQuantity = async (itemId, size, quantity) => {
+
+        let cartData = structuredClone(cartItems)
+
+        cartData[itemId][size] = quantity;
+
+        setCartItems(cartData)
+    }
 
     const value = {
-        products, currency, delivery_fee
+        products, currency, delivery_fee,
+        search, setSearch, showSearch, setShowSearch,
+        cartItems, setCartItems, addToCart,
+        getCartCount, updateQuantity
     }
 
     return(
