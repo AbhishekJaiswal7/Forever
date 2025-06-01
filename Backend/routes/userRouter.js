acbbb756b9e10d17bca2c14a258cc9edf2f87836
Abhishek <abhishekjaiswal1788@gmail.com>
2025-06-01 20:35:00 +0530

    add admin login route to user router

--- Backend/routes/userRouter.js
+++ Backend/routes/userRouter.js
@@ -1,9 +1,10 @@
-import { loginUser, registerUser } from "../controllers/user.js";
+import { loginUser, registerUser, adminLogin } from "../controllers/user.js";
 import express from "express";
 
 const router = express.Router();
 
 router.post("/login", loginUser);
 router.post("/register", registerUser);
+router.post("/admin", adminLogin)
 
 export default router;
